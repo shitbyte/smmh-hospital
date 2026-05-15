77d7273fe6c10f531427695b2896545104f7bd92
shitbyte <mohsinjawad1402@email.com>
2026-05-15 19:17:54 +0500

    Add animations, department icons, image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'ejdwlpoymbvxvbgwfimu.supabase.co',
+      'www.sairamirajhospital.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
+
